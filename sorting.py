3a3c987b99514fa41a7b8161d30b9d754f8ae67d
s1967340 <s1967340@ed.ac.uk>
2020-01-15 12:39:37 +0000

    add comment to sorting.py

--- sorting.py
+++ sorting.py
@@ -5,12 +5,12 @@ arr=np.random.random(100)
 
 def sort(arr):
     t=time.clock()
-    for i in range(0,100):
-        for j in range(0,100):
+    for i in range(0,len(arr)):
+        for j in range(0,len(arr)):
             if arr[i] < arr[j]:
-                moving = arr[i]
-                arr = np.delete(arr, i)
-                arr = np.insert(arr, j, moving)
+                moving = arr[i] # save the i-th value which will be delete in the next line
+                arr = np.delete(arr, i) # delete i-th value
+                arr = np.insert(arr, j, moving) # put the i-th value in front of j-th value
     t=time.clock() - t
     print(str(t))
     return arr
